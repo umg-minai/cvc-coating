d93235e0b934995206b3b92d0b1a8fe2ca41575a
Sebastian Gibb <mail@sebastiangibb.de>
2024-10-17 09:20:07 +0200

    chore: add channel introduction for minai

--- guix/channels.pinned.scm
+++ guix/channels.pinned.scm
@@ -3,13 +3,18 @@
         (url "https://github.com/umg-minai/guix-minai.git")
         (branch "main")
         (commit
-          "b8f25c02ecfdc3b36d35291ea33e2abb2634da15"))
+          "b8f25c02ecfdc3b36d35291ea33e2abb2634da15")
+        (introduction
+          (make-channel-introduction
+            "4e47c0f12cf838335bf731b1947ea434923569e3"
+            (openpgp-fingerprint
+              "7EAB 7082 91D9 28E5 4387  47A7 F9F3 CB1B 50FB 607A"))))
       (channel
         (name 'guix)
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "b3b98151f30552357b91154deb6cc71696af5537")
+          "d95588242c605fbb72e25fe36a0903a1538e9018")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
