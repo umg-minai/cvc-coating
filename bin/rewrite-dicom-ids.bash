6eaba89ba158298a0d2b0885c8efe674ad7ef557
Sebastian Gibb <mail@sebastiangibb.de>
2025-12-28 22:32:57 +0100

    feat: use DICOM StudyID

--- bin/rewrite-dicom-ids.bash
+++ bin/rewrite-dicom-ids.bash
@@ -14,27 +14,40 @@ cd ${DICOMDIR}
 for DICOMFILE in $(find ${DICOMSUBDIR} -type f,l); do
   DUMPID=$(dcmdump --search PatientID --search-first ${DICOMFILE})
   DUMPPN=$(dcmdump --search PatientName --search-first ${DICOMFILE})
-  OID=$(echo "${DUMPID}" | sed '/^.*\([[:alpha:]]\{2,3\}[0-9][0-9][-_.]\{1,2\}d\?[0-9]\{1,2\}[^]]*\).*$/!d; s//\1/')
-  OPN=$(echo "${DUMPPN}" | sed '/^.*\([[:alpha:]]\{2,3\}[0-9][0-9][-_.]\{1,2\}d\?[0-9]\{1,2\}[^]]*\).*$/!d; s//\1/')
+  DUMPSD=$(dcmdump --search StudyID --search-first ${DICOMFILE})
+  OID=$(echo "${DUMPID}" | sed '/^.*\([[:alpha:]]\{2,3\}[0-9][0-9][-_.]\{1,2\}d\?[0-9]\{1,2\}[^]]*\)(\?.*$/!d; s//\1/')
+  OPN=$(echo "${DUMPPN}" | sed '/^.*\([[:alpha:]]\{2,3\}[0-9][0-9][-_.]\{1,2\}d\?[0-9]\{1,2\}[^]]*\)(\?.*$/!d; s//\1/')
+  OSD=$(echo "${DUMPSD}" | sed '/^.*\([[:alpha:]]\{2,3\}[0-9][0-9][-_.]\{1,2\}d\?[0-9]\{1,2\}[^]]*\)(\?.*$/!d; s//\1/')
   ID=$(echo ${OID} | sed 's#[-_.]\+#-#; s#^\(.*\)$#\U\1#; s#-D#-#; s#-\([0-9]\)$#-0\1#')
   PN=$(echo ${OPN} | sed 's#[-_.]\+#-#; s#^\(.*\)$#\U\1#; s#-D#-#; s#-\([0-9]\)$#-0\1#')
+  SD=$(echo ${OSD} | sed 's#[-_.]\+#-#; s#^\(.*\)$#\U\1#; s#-D#-#; s#-\([0-9]\)$#-0\1#')
 
-  if [ -z "${ID}" -a -z "${PN}" ] ; then
-    echo "${DICOMFILE}: error - neither ID nor Name valid"; exit 1
-  elif [ -z "${PN}" ] ; then
+  if [ -z "${SD}" -a -z "${ID}" -a -z "${PN}" ] ; then
+    echo "${DICOMFILE}: error - neither StudyID, PatientID nor PatientName valid"
+    echo "${DICOMFILE}:"
+    echo "SD: ${DUMPSD}"
+    echo "ID: ${DUMPID}"
+    echo "PN: ${DUMPPN}"
+    exit 1
+  elif [ -z "${PN}" -a -z "${SD}" ] ; then
     # ID valid
-    PN=${ID}
-  else
+    SD=${ID}
+  elif [ -z "${ID}" -a -z "${SD}" ] ; then
     # PN valid
-    ID=${PN}
+    SD=${PN}
   fi
 
-  if [ "${OID}" = "${ID}" -a "${OPN}" = "${PN}" ] ; then
-    echo "${DICOMFILE}: [ ] keep ${ID}"
+  ID=$(echo ${SD} | cut -d- -f1)
+
+  if [ "${OSD}" = "${SD}" ] ; then
+    echo "${DICOMFILE}: [ ] keep ${ID}/${SD}"
   else
-    echo "${DICOMFILE}: [M] set ${ID} (before ID:${OID}/PN:${OPN})"
-    dcmodify --erase-all PatientID --erase-all PatientName --erase-private \
-      --insert "(0010,0010)=${PN}" --insert "(0010,0020)=${PN}" \
+    echo "${DICOMFILE}: [M] set ${ID}/${SD} (was ID:${OID}/PN:${OPN}/SD:${OSD})"
+    dcmodify --erase-all PatientID --erase-all PatientName \
+      --erase-all StudyID \
+      --erase-private \
+      --insert "(0010,0010)=${ID}" --insert "(0010,0020)=${ID}" \
+      --insert "(0020,0010)=${SD}" \
       --no-backup ${DICOMFILE}
   fi
 done
